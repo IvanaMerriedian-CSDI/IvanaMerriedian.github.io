06040c640d8027510064287d40f8cdc811b7d75c
IvanaMerriedian <ivanabella99@gmail.com>
2019-11-21 11:25:59 +0700

    hai

---
+++ precache-manifest.c769ac6073d99a4a4683d9d27df99dd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99943486de9c5008bcc7",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "53b1840cfcb09a67785c",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "a87e41c368cd30ec23e1bc1241034560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99943486de9c5008bcc7",
+    "url": "/js/about.ff732dc7.js"
+  },
+  {
+    "revision": "0de1b95059f9bb7012d7",
+    "url": "/js/app.f4ff4eb4.js"
+  },
+  {
+    "revision": "53b1840cfcb09a67785c",
+    "url": "/js/chunk-vendors.7cf797c4.js"
+  },
+  {
+    "revision": "573c4900d799db5de0f0e8f7c52885b3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
